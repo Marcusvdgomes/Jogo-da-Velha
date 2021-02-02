445ff5facce124a219ec22f1470d41e05cb8da1f
Marcusvdgomes <50336885+Marcusvdgomes@users.noreply.github.com>
2021-02-02 13:43:58 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Square from "./components/Square/Square";
+import Board from "./components/Board/Board";
+import Game from "./components/Game/Game";
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
